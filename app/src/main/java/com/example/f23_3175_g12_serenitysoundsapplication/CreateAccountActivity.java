8a878eefbc62b20b56e0ff612fc3c682e2fdbc2a
Sano <sanot@student.douglascollege.ca>
2023-11-07 20:48:25 -0800

    layout1 attempt

---
+++ app/src/main/java/com/example/f23_3175_g12_serenitysoundsapplication/CreateAccountActivity.java
@@ -0,0 +1,6 @@
+package com.example.f23_3175_g12_serenitysoundsapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateAccountActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/f23_3175_g12_serenitysoundsapplication/LoginActivity.java
@@ -0,0 +1,7 @@
+package com.example.f23_3175_g12_serenitysoundsapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+}
