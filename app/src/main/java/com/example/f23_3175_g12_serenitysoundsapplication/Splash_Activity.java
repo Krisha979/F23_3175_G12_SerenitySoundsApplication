473f74919bbdc6bb51f9dd03024fc5d5ac4f108e
Krisha Mahat <krishamahat751@gmail.com>
2023-11-11 12:29:37 -0500

    "feat: Splash screen"

--- app/src/main/java/com/example/f23_3175_g12_serenitysoundsapplication/Splash_Activity.java
+++ app/src/main/java/com/example/f23_3175_g12_serenitysoundsapplication/Splash_Activity.java
@@ -2,13 +2,26 @@ package com.example.f23_3175_g12_serenitysoundsapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splash_Activity extends AppCompatActivity {
+    private static final int SPLASH_DELAY = 5000; // 2 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        // Delay and navigate to the main activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash_Activity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DELAY);
     }
 }
