4cc044a707076a269b6d2b8c60586cb6d28840d4
Krisha Mahat <krishamahat751@gmail.com>
2023-11-11 12:03:42 -0500

    "feat: Navigation Bar on home page"

--- app/src/main/java/com/example/f23_3175_g12_serenitysoundsapplication/MainActivity.java
+++ app/src/main/java/com/example/f23_3175_g12_serenitysoundsapplication/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.f23_3175_g12_serenitysoundsapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +17,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
     }
 
+    private BottomNavigationView.OnItemSelectedListener navListener =
+            new BottomNavigationView.OnItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    if (item.getItemId() == R.id.navigation_home) {
+                        selectedFragment = new HomeFragment();
+                    } else if (item.getItemId() == R.id.navigation_music) {
+                        selectedFragment = new MusicFragment();
+                    } else if (item.getItemId() == R.id.navigation_location) {
+                        selectedFragment = new LocationFragment();
+                    } else if (item.getItemId() == R.id.navigation_profile) {
+                        selectedFragment = new ProfileFragment();
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
     public void openLoginPage(View view){
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
@@ -32,6 +64,4 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ScheduleActivity.class);
         startActivity(intent);
     }
-
-
 }
---
+++ app/src/main/java/com/example/f23_3175_g12_serenitysoundsapplication/Splash_Activity.java
@@ -0,0 +1,14 @@
+package com.example.f23_3175_g12_serenitysoundsapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Splash_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
