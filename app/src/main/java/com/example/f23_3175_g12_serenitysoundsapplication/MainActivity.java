f156baf8048acbf191410d5ec65c54526fac8ecc
Krisha Mahat <krishamahat751@gmail.com>
2023-11-12 01:29:02 -0500

    "feat: navigation drawer"

--- app/src/main/java/com/example/f23_3175_g12_serenitysoundsapplication/MainActivity.java
+++ app/src/main/java/com/example/f23_3175_g12_serenitysoundsapplication/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.f23_3175_g12_serenitysoundsapplication;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
@@ -10,13 +14,44 @@ import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle toggle;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find views
+        drawer = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        // Set up the Toolbar as your transparent action bar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Set up the Navigation Drawer
+        toggle = new ActionBarDrawerToggle(
+                this,
+                drawer,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+// Handle item clicks in the Navigation Drawer
+        navigationView.setNavigationItemSelectedListener(item -> {
+            // Handle navigation item clicks here
+            // For example, you can switch fragments or start activities
+            return true;
+        });
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnItemSelectedListener(navListener);
 
